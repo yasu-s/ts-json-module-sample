29fbc71869a9896b9fad00ed1011d18bfb1365fd
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-12-02 21:04:30 +0900

    初期設定

---
+++ dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const data_json_1 = __importDefault(require("./data.json"));
+console.log(`id=${data_json_1.default.id}, memo=${data_json_1.default.memo}`);
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import data from './data.json';
+
+console.log(`id=${data.id}, memo=${data.memo}`);
